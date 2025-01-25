769a71e1f33ac2ffe6cb8c9d1c9d0252ff6dd6c9
Yernazar Rakhmetov <59398787+achynya@users.noreply.github.com>
2025-01-25 21:36:48 +0500

    Add files via upload

---
+++ assignment3/README.md
@@ -0,0 +1,29 @@
+# Hospital OOP (Java) Project
+
+This project is a simple **Hospital Management System** built using Object-Oriented Programming principles in Java. It simulates the management of patients and doctors in a hospital, providing features such as adding patients and doctors, sorting them, and editing their details.
+
+## Assignment Details
+
+This assignment was completed as part of the coursework for **Astana IT University**. It demonstrates the use of **Java OOP principles** including classes, inheritance, encapsulation, and polymorphism.
+
+## Features
+
+- Add Patients
+- Add Doctors
+- View Patients and Doctors Lists
+- Sort Patients and Doctors by Name or Age
+- Edit Patient and Doctor Data
+
+## Installation
+
+To run this project, follow these steps:
+
+1. Run your IDLE 
+
+2. Clone the repository to your local machine or just download them from GitHub:
+   git clone https://github.com/achynya/assignment3.git
+3. Run main file and enjoy of usage.
+
+## Additionally 
+
+Code have 5-second delay everytime you have the entering begin choice.
---
+++ assignment3/src/Person.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    @Override
+    public String toString() {
+        return "Person{name='" + name + '\'' + ", age=" + age + ", gender='" + gender + '\'' + '}';
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return age == person.age && name.equals(person.name) && gender.equals(person.gender);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, gender);
+    }
+}
